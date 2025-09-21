5e4a15b7faae33b8a5791b8524d28a5ac9709c75
nowelbaez2017-ux <nowelbaez2017@gmail.com>
2025-09-21 17:27:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scoreboard.Web.Data;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios MVC
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Pipeline HTTP
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // ✅
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
